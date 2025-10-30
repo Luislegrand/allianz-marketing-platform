bd205729a7e5c60b1d157c1cf5a54e1df631e431
Luislegrand <lfelipesfreitas@gmail.com>
2025-10-30 19:20:24 -0300

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,8 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './layouts/**/*.{js,ts,jsx,tsx,mdx}',
-    './styles/**/*.{css}',
-    './**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './styles/**/*.css',
   ],
   theme: {
     extend: {},
