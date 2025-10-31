d8db9817edbaee52231dddb24c0d96b735b7a986
Luislegrand <lfelipesfreitas@gmail.com>
2025-10-31 19:53:38 -0300

    metrics.js

---
+++ pages/dashboard/agency/metrics.js
@@ -0,0 +1,4 @@
+import Header from '../../../components/Header'
+import Footer from '../../../components/Footer'
+import { useAllianz } from '../../../context/AllianzContext'
+import { useRouter } from 'next/router'
