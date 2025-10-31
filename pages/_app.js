b71fe8a2d78071f9d9eb2b02ffed2cf5393e69fa
Luislegrand <lfelipesfreitas@gmail.com>
2025-10-31 19:38:23 -0300

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+// pages/_app.js
 import '../styles/globals.css'
+import { AllianzProvider } from '../context/AllianzContext'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AllianzProvider>
+      <Component {...pageProps} />
+    </AllianzProvider>
+  )
 }
