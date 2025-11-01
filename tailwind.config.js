5a4e9c6ba0ad99e9669f5d6a160a672720610c60
Luislegrand <lfelipesfreitas@gmail.com>
2025-11-01 12:35:16 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './styles/**/*.css',
+    content: [
+  './pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './components/**/*.{js,ts,jsx,tsx,mdx}',
+  './context/**/*.{js,ts,jsx,tsx,mdx}',
+  './styles/**/*.css',
   ],
   theme: {
     extend: {},
