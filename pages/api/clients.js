929a57bbf90b1887748637f36770263f765888a7
Luislegrand <lfelipesfreitas@gmail.com>
2025-10-31 21:12:21 -0300

    Update clients.js

--- pages/api/clients.js
+++ pages/api/clients.js
@@ -2,18 +2,16 @@ import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
   const client = await clientPromise;
-  const db = client.db("allianz"); // nome do banco que você definiu no .env.local
+  const db = client.db("allianz");
   const collection = db.collection("clients");
 
   try {
     if (req.method === "GET") {
-      // Retorna todos os clientes
       const clients = await collection.find({}).toArray();
       return res.status(200).json(clients);
     }
 
     if (req.method === "POST") {
-      // Cria um novo cliente
       const newClient = req.body;
 
       if (!newClient || !newClient.email) {
@@ -40,4 +38,3 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: "Erro interno no servidor" });
   }
 }
-
